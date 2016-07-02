b022a848ef0d076c013d21d73daf5f4f258a712e
Nick Dyer <nbdyer@gmail.com>
2016-07-02 16:20:56 +0100

    Reduce sum working slightly better code

--- test/nickbdyer/uk/MoneyTest.java
+++ test/nickbdyer/uk/MoneyTest.java
@@ -67,6 +67,7 @@ public class MoneyTest {
     //$5 + 10 CHF = $10 if rate is 2:1
     //$5 + $5 = $10
     //Return Money from $5 + $5
+    //Bank.reduce(Money)
     //Money rounding
     //hashCode()
     //Equal null

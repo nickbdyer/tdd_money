62e4bd6c98583e4eab6caa8a255f0cb2c897a2d6
Nick Dyer <nbdyer@gmail.com>
2016-07-02 14:13:16 +0100

    Generalises equals method and moves to Money class

--- src/nickbdyer/uk/Money.java
+++ src/nickbdyer/uk/Money.java
@@ -4,4 +4,9 @@ public class Money {
 
     protected int amount;
 
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
+
 }

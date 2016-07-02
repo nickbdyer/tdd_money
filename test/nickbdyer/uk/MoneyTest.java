02b8a190747f514cb0a4cfe363da2e19186982d4
Nick Dyer <nbdyer@gmail.com>
2016-07-02 13:19:38 +0100

    Passing test for equality

--- test/nickbdyer/uk/MoneyTest.java
+++ test/nickbdyer/uk/MoneyTest.java
@@ -3,6 +3,7 @@ package nickbdyer.uk;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MoneyTest {
 
@@ -15,9 +16,21 @@ public class MoneyTest {
         assertEquals(15, product.amount);
     }
 
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+    }
+
+    //TO DO
+
     //$5 + 10 CHF = $10 if rate is 2:1
     //make "amount" private
-    //Dollar side effects?
     //Money rounding
+    //equals
+
+    //DONE
+
+    //$5 * 2 = $10
+    //Dollar side effects?
 
 }

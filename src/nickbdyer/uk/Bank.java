4fc529e1257184a53857fb1dc21c8931c5321b88
Nick Dyer <nbdyer@gmail.com>
2016-07-02 16:22:34 +0100

    Passing test for bank reduce money, pre-refactor

--- src/nickbdyer/uk/Bank.java
+++ src/nickbdyer/uk/Bank.java
@@ -3,6 +3,7 @@ package nickbdyer.uk;
 public class Bank {
 
     Money reduce(Expression source, String to) {
+        if (source instanceof Money) return (Money) source;
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }

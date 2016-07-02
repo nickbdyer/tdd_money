558fa10c4dbcb3f97a9ef92f5a4db691b3f4730c
Nick Dyer <nbdyer@gmail.com>
2016-07-02 13:05:04 +0100

    Adds test notes, and starts to attack the dollar side effects in tests

--- test/nickbdyer/uk/MoneyTest.java
+++ test/nickbdyer/uk/MoneyTest.java
@@ -11,6 +11,13 @@ public class MoneyTest {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
+        five.times(3);
+        assertEquals(15, five.amount);
     }
+
+    //$5 + 10 CHF = $10 if rate is 2:1
+    //make "amount" private
+    //Dollar side effects?
+    //Money rounding
 
 }

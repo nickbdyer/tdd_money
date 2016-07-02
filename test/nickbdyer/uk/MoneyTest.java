df1d8656a9f8a3386484e774649765529f240113
Nick Dyer <nbdyer@gmail.com>
2016-07-02 16:50:54 +0100

    PAssing test for adding mixed currencies

--- test/nickbdyer/uk/MoneyTest.java
+++ test/nickbdyer/uk/MoneyTest.java
@@ -81,6 +81,16 @@ public class MoneyTest {
         assertEquals(1, new Bank().rate("USD", "USD"));
     }
 
+    @Test
+    public void testMixedAddition() {
+        Money fiveBucks = Money.dollar(5);
+        Money tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
+
     //TO DO
 
     //$5 + 10 CHF = $10 if rate is 2:1

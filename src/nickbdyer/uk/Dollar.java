f8597abd6f17bac4e59fcba7292de2cc0bbd46ac
Nick Dyer <nbdyer@gmail.com>
2016-07-02 15:07:22 +0100

    Inline factory creation methods for times method

--- src/nickbdyer/uk/Dollar.java
+++ src/nickbdyer/uk/Dollar.java
@@ -9,7 +9,7 @@ public class Dollar extends Money{
     }
 
     Money times(int multiplier) {
-        return Money.dollar(amount * multiplier);
+        return new Dollar(amount * multiplier, "USD");
     }
 
     String currency() {
--- src/nickbdyer/uk/Franc.java
+++ src/nickbdyer/uk/Franc.java
@@ -9,7 +9,7 @@ public class Franc extends Money {
     }
 
     Money times(int multiplier) {
-        return Money.franc(amount * multiplier);
+        return new Franc(amount * multiplier, "CHF");
     }
 
     String currency() {

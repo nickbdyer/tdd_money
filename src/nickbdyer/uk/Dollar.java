df5dc1147d9f2d5393def1be4d941d5d47958f6a
Nick Dyer <nbdyer@gmail.com>
2016-07-02 12:17:08 +0100

    Adds first compiling test

---
+++ src/nickbdyer/uk/Dollar.java
@@ -0,0 +1,15 @@
+package nickbdyer.uk;
+
+public class Dollar {
+
+    int amount;
+
+    Dollar(int amount) {
+
+    }
+
+    void times(int multiplier) {
+
+    }
+
+}
---
+++ test/nickbdyer/uk/MoneyTest.java
@@ -0,0 +1,16 @@
+package nickbdyer.uk;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MoneyTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+
+}

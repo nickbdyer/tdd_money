58ee85b8f36f4ec7a6de6ae7a1756d27b6453578
Nick Dyer <nbdyer@gmail.com>
2016-07-02 14:09:11 +0100

    Adds new test for franc multiplcation, copy and paste of dollar implemenation to make pass, needs refactor

--- test/nickbdyer/uk/MoneyTest.java
+++ test/nickbdyer/uk/MoneyTest.java
@@ -21,19 +21,30 @@ public class MoneyTest {
         assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
     //TO DO
 
     //$5 + 10 CHF = $10 if rate is 2:1
-    //make "amount" private
     //Money rounding
     //hashCode()
     //Equal null
     //Equal object
+    //Dollar/Franc duplcation
+    //Common equals
+    //Common times
 
     //DONE
 
     //$5 * 2 = $10
     //Dollar side effects?
     //equals
+    //make "amount" private
+    //5CHF * 2 = 10CHF
 
 }

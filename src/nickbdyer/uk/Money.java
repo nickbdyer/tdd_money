0384e0e702abdd8fa4122efd248f094b9e85a35a
Nick Dyer <nbdyer@gmail.com>
2016-07-02 14:35:30 +0100

    Decoupled test code from concrete subclasses, some tests redundant, step towards common times

--- src/nickbdyer/uk/Money.java
+++ src/nickbdyer/uk/Money.java
@@ -13,6 +13,10 @@ abstract class Money {
         return new Dollar(amount);
     }
 
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
     abstract Money times(int multiplier);
 
 }

64928651ae74423fdaa5433db85b989ccc29fcfd
Nick Dyer <nbdyer@gmail.com>
2016-07-02 13:07:04 +0100

    New test compiles, but nullpoint exception

--- test/nickbdyer/uk/MoneyTest.java
+++ test/nickbdyer/uk/MoneyTest.java
@@ -9,10 +9,10 @@ public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
-        five.times(3);
-        assertEquals(15, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 
     //$5 + 10 CHF = $10 if rate is 2:1

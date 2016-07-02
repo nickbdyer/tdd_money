c3159412606fe7ef178ff1dd2b7d0738f12dc231
Nick Dyer <nbdyer@gmail.com>
2016-07-02 16:31:26 +0100

    Horrible implementation of bank rate within money class

--- src/nickbdyer/uk/Bank.java
+++ src/nickbdyer/uk/Bank.java
@@ -6,4 +6,8 @@ public class Bank {
         return source.reduce(to);
     }
 
+    void addRate(String from, String to, int amount) {
+
+    }
+
 }
--- test/nickbdyer/uk/MoneyTest.java
+++ test/nickbdyer/uk/MoneyTest.java
@@ -68,11 +68,20 @@ public class MoneyTest {
         assertEquals(Money.dollar(1), result);
     }
 
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
     //TO DO
 
     //$5 + 10 CHF = $10 if rate is 2:1
     //$5 + $5 = $10
     //Return Money from $5 + $5
+    //Reduce Money with conversion
     //Money rounding
     //hashCode()
     //Equal null
